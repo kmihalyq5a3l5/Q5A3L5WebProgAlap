9905c27c5517d665ece3465138f4eedf3cfb037e
kmihalyq5a3l5 <mihaly8k@gmail.com>
2025-11-20 10:21:07 +0100

    18.Pract

---
+++ Q5A3L5_1120/Q5A3L5_orarend.js
@@ -0,0 +1,14 @@
+{
+    "Q5A3L5_orarend": {
+        "ora": [
+            {
+                "targy": "Programozás alapjai" {
+                    "nap": "hetfo",
+                    "tol": 8,
+                    "ig" : 11:30
+                },
+                "helyszin"
+            }
+        ]
+    }
+}
